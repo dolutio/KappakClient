cc28d442319820e00d7033334d181ca7828272a1
LightWay <98546746+dolutio@users.noreply.github.com>
2025-07-12 04:51:34 +0000

    Update

--- kappak_events.py
+++
@@ -1,9 +0,0 @@
-current_event = None
-
-class InteractiveEvent:...
-
-class OnSendButtonReleased(InteractiveEvent):
-    ...
-
-def _on_SendButton_released():
-    pass
--- kappak_general_functions.py
+++
@@ -1,9 +0,0 @@
-from kivy.core.window import Window
-
-from kappak_config import window_height, window_width
-
-def return_values(value, count_of_return_values):
-    return tuple(value for _ in range(count_of_return_values))
-
-def adaptive_size(value):
-    return value / window_width * Window.height
